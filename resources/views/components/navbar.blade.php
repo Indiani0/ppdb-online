13f35c1c8a5bd3d3af29a8d8dbc277990504a50a
Indi <srirahayu200207@gmail.com>
2024-08-07 00:26:49 +0700

    update view navbar authentication role

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,15 @@
 <nav class="navbar top-bar">
     <h1 class="judul"><a href="/">PPDB SMK Piramida</a></h1>
     <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/login">Login</a></li>
+        @auth
+            @if (Auth::user()->role_id === 3)
+                <li><a href="{{ route('beranda') }}">Home</a></li>
+                <li><a href="{{ route('form') }}">Formulir</a></li>
+                <li><a href="{{ route('login') }}">Login</a></li>
+            @endif
+        @else
+            <li><a href="{{ route('beranda') }}">Home</a></li>
+            <li><a href="{{ route('login') }}">Login</a></li>
+        @endauth
     </ul>
 </nav>
