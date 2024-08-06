8ef9ac7983b197f8941052ea4e3037c0e7b4a13d
Indi <srirahayu200207@gmail.com>
2024-08-06 23:48:57 +0700

    update view navbar & RegisterController

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,7 +2,6 @@
     <h1 class="judul"><a href="/">PPDB SMK Piramida</a></h1>
     <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/form">Formulir PPDB</a></li>
         <li><a href="/login">Login</a></li>
     </ul>
 </nav>
