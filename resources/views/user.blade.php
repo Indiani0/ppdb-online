220e069a9a9525bfd23603a39ed3382f3a813c44
Indi <srirahayu200207@gmail.com>
2024-08-08 17:07:54 +0700

    add confirmation for EDIT & DELETE Data User

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -32,9 +32,12 @@
                                 <td>{{ $user->name }}</td>
                                 <td>{{ $user->email }}</td>
                                 <td>{{ $user->role->role }}</td>
-                                <td><a href="{{ route('users.edit', $user->id) }}" class="btn btn-warning">Edit</a></td>
+                                <td><a href="{{ route('users.edit', $user->id) }}" class="btn btn-warning"
+                                        onclick="return confirm('Apakah Anda yakin ingin mengubah data user ini?');">Edit</a>
+                                </td>
                                 <td>
-                                    <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+                                    <form action="{{ route('users.destroy', $user->id) }}" method="POST"
+                                        onsubmit="return confirm('Apakah Anda yakin ingin menghapus data user ini?');">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-danger">Delete</button>
