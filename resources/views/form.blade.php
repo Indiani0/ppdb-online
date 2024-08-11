c99d9f591a00bc3837095261269838e8c226e869
Indi <srirahayu200207@gmail.com>
2024-08-12 00:09:46 +0700

    Revert "add confirmation to URL WhatsApp"
    This reverts commit f78ed8a4af76e39cd2f1e0418816a69176f4d105.

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -28,6 +28,11 @@
                 @if (session('success'))
                     <div class="alert alert-success">
                         {{ session('success') }}
+
+                        @if (session('whatsappUrl'))
+                            <a href="{{ session('whatsappUrl') }}" class="btn btn-success" target="_blank">Upload
+                                Dokumen Pendukung Disini</a>
+                        @endif
                     </div>
                 @endif
 
