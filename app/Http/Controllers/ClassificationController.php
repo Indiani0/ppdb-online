c36e937c97876dab8eb0ffabcc5f807a8f2e0510
Indi <srirahayu200207@gmail.com>
2024-08-12 22:02:04 +0700

    add configuration for C45 Algorithm (error)

--- app/Http/Controllers/ClassificationController.php
+++ app/Http/Controllers/ClassificationController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Algorithm\C45;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use App\Models\Classification;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class ClassificationController extends Controller
 {
@@ -23,6 +28,34 @@ class ClassificationController extends Controller
      */
     public function index()
     {
-        return view('classification');
+        // return view('classification');
+
+        // Ambil data dari database
+        $data = Student::all();
+
+        // Konversi data ke format yang sesuai dengan C4.5
+        $c45Data = $data->toArray();
+
+        // Inisialisasi C4.5
+        $c45 = new C45($c45Data, ['attribute1', 'attribute2'], 'class');
+        $tree = $c45->buildTree();
+
+
+        // Tentukan atribut target dan atribut lainnya
+        $targetAttribute = 'status_kelulusan';
+        $attributes = ['nilai_mtk', 'nilai_ipa', 'jenis_kelamin'];
+
+        // Bangun pohon keputusan
+        $tree = $c45->buildTree($targetAttribute, $attributes);
+
+        // Lakukan klasifikasi pada data baru
+        $newData = [
+            'nilai_mtk' => 80,
+            'nilai_mtk' => 87,
+            'jenis_kelamin' => 'Laki-laki',
+        ];
+        $classification = $c45->classify($tree, $newData);
+
+        return view('classification.result', ['classification' => $classification]);
     }
 }
--- resources/views/classification.blade.php
+++ resources/views/classification.blade.php
@@ -4,8 +4,16 @@
     <div class="container">
         <div class="card">
             <div class="card-body">
-                <h1>Selamat Datang di Halaman Data Klasifikasi</h1>
+                <h1>Selamat Datang di Halaman Klasifikasi C4.5</h1>
                 <p>Halaman ini berisi proses klasifikasi menggunakan Algotitma C4.5</p>
+
+                @if (!empty($tree))
+                    <h1>Pohon Keputusan</h1>
+                    <pre>{{ var_dump($tree, true) }}</pre>
+                @else
+                    <p>Tidak ada pohon keputusan yang dihasilkan.</p>
+                @endif
+
             </div>
         </div>
     </div>
