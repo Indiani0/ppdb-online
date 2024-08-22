b056fab10bddce8e01d576acdcf35751bedebe94
Indi <srirahayu200207@gmail.com>
2024-08-22 20:26:47 +0700

    tidy up the code Models User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
